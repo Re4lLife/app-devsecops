9999be7cbdbfd8a4b27db3ad5be1d8eae79c9c6f
Re4lLife <osokorfortune@gmail.com>
2026-06-21 07:54:18 +0100

    Added  SBOM generation and code fixes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,8 +3,3 @@ output "alb_dns_name" {
   value       = aws_lb.main.dns_name
 }
 
-output "rds_endpoint" {
-  description = "RDS endpoint stored in Secrets Manager - no manual action needed"
-  value       = aws_db_instance.mysql.address
-  sensitive   = true
-}
